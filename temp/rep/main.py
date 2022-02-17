8ae147e560b6599019eed73363ce20d5c692ac99
Caio Barros <caiobarrosufc@gmail.com>
2022-02-17 19:29:23 -0300

    updated

--- temp/rep/main.py
+++ temp/rep/main.py
@@ -8,6 +8,30 @@ COLOR_RED = (0, 0, 255)
 COLOR_GREEN = (0, 255, 0)
 COLOR_BLUE = (255, 0, 0)
 
+def preprocessing(img):
+    # conversao da imagem para escala de cinza
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # aplicacao de filtro gaussiano
+    imgblur = cv.GaussianBlur(img_gray, (3,3), 0)
+    # aplicacao de filtro laplaciano
+    imglap = cv.Laplacian(imgblur, cv.CV_16S, ksize=3)
+    # conversao para uint8
+    imgabs = cv.convertScaleAbs(imglap)
+    # equalizacao de histograma
+    imgeq = cv.equalizeHist(imgabs)
+    # filtro de media
+    imgblur = cv.blur(imgeq, (5,5))
+    # limiarizacao binaria
+    ret,thresh1 = cv.threshold(imgblur,120,255,cv.THRESH_BINARY_INV)
+    # opening
+    kernel = np.ones((13,13),np.uint8)
+    opening = cv.morphologyEx(thresh1, cv.MORPH_OPEN, kernel)
+    # dilatacao
+    kernel_dl = np.ones((3,3),np.uint8)
+    dilation = cv.dilate(opening,kernel_dl,iterations = 1)
+    
+    return dilation
+
 def detect(file):
     output = cv.imread(file)
     jeans = cv.cvtColor(output, cv.COLOR_BGR2GRAY)
